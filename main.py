f8fc23274426daaa2513bc6da5cff49ce183cda3
jose rasgado <jrasgado01@hotmail.com>
2024-04-18 21:05:30 -0600

    Send email with titles and descriptions

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import requests
+from send_email import send_email
 
 api_key = "4bbb10d45f86466485e4eeb669ce2a0e"
-url = ("https://newsapi.org/v2/everything?q=tesla&"
-       "from=2024-03-18&sortBy=publishedAt&apiKey="
-       "4bbb10d45f86466485e4eeb669ce2a0e")
+url = ("https://newsapi.org/v2/everything?q=apple&from=2024-04-18&to=2024-04-18&sortBy=popularity&apiKey=4bbb10d45f86466485e4eeb669ce2a0e")
 
 # Make request
 request = requests.get(url)
@@ -11,8 +10,14 @@ request = requests.get(url)
 # Get a dictionary with data
 content = request.json()
 
+message = ""
 # Access the article titles and description
 for article in content["articles"]:
-    print(article["title"])
-    print(article["description"])
+    if article["title"] is not None:
+        message = message + article["title"] + "\n" + article["description"] + 2*"\n"
+        # print("Title: ", article["title"])
+        # print("Description: ", article["description"])
+
+message = message.encode("UTF-8")
+send_email(message)
 
---
+++ send_email.py
@@ -0,0 +1,16 @@
+import smtplib, ssl
+import os
+
+
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+    username = "jrasgado01@gmail.com"
+    password = "nzma ozxb nyom zcsu"
+
+    receiver = "jrasgado01@hotmail.com"
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
